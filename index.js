295d493123e690c970235386e88cc3d4a1ee541f
Janzo <giovanniberi93@gmail.com>
2017-06-03 00:49:04 +0200

    [INDEX.JS] Sketch of the APIs, woring database
    Signed-off-by: Janzo <giovanniberi93@gmail.com>

--- index.js
+++ index.js
@@ -14,6 +14,11 @@ const _ = require("lodash");
 ////////////////// INIT DB //////////////////
 /////////////////////////////////////////////
 
+// get json files that contains data to populate db
+let doctorsList = require("./other/doctorsdata.json");
+let locationsList = require("./other/locationsdata.json");
+let servicesList = require("./other/servicesdata.json");
+let servicesLocationsList = require("./other/serviceslocationsdata.json");
 
 const sqlDb = sqlDbFactory({
   client: "sqlite3",
@@ -23,17 +28,123 @@ const sqlDb = sqlDbFactory({
   }
 });
 
-function initDb() {
- // TODO
+
+function initDoctorsTable() {
+  return sqlDb.schema.hasTable("doctors").then(exists => {
+    if (!exists) {
+      sqlDb.schema
+        .createTable("doctors", table => {
+          // create the table
+          table.increments("id").primary();
+          table.string("name");
+          table.string("surname");
+          table.string("location");
+          table.string("basicInfo");
+          table.string("service");
+          table.boolean("isResponsible");
+        })
+        .then(() => {
+          return Promise.all(
+            _.map(doctorsList, p => {
+              // insert the row
+              return sqlDb("doctors").insert(p);
+            })
+          );
+        });
+    } else {
+      return true;
+    }
+  });
+}
+
+function initLocationsTable() {
+  return sqlDb.schema.hasTable("locations").then(exists => {
+    if (!exists) {
+      sqlDb.schema
+        .createTable("locations", table => {
+          // create the table
+          table.increments("id").primary();
+          table.string("name");
+          table.string("basicInfo");
+          table.string("contacts");
+        })
+        .then(() => {
+          return Promise.all(
+            _.map(locationsList, p => {
+              // insert the row
+              return sqlDb("locations").insert(p);
+            })
+          );
+        });
+    } else {
+      return true;
+    }
+  });
+}
+
+function initServicesTable() {
+  return sqlDb.schema.hasTable("services").then(exists => {
+    if (!exists) {
+      sqlDb.schema
+        .createTable("services", table => {
+          // create the table
+          table.increments("id").primary();
+          table.string("name");
+          table.string("description");
+          table.string("treatment");
+        })
+        .then(() => {
+          return Promise.all(
+            _.map(servicesList, p => {
+              // insert the row
+              return sqlDb("services").insert(p);
+            })
+          );
+        });
+    } else {
+      return true;
+    }
+  });
+}
+
+function initServicesLocationsTable() {
+  return sqlDb.schema.hasTable("servicesLocations").then(exists => {
+    if (!exists) {
+      sqlDb.schema
+        .createTable("servicesLocations", table => {
+          // create the table
+          table.integer("idService");
+          table.integer("idLocation");
+          table.primary(["idService", "idLocation"]);
+          // set both as foreign key
+          table.foreign("idService").references("services.id");
+          table.foreign("idLocation").references("locations.id");
+        })
+        .then(() => {
+          return Promise.all(
+            _.map(servicesLocationsList, p => {
+              // insert the row
+              return sqlDb("servicesLocations").insert(p);
+            })
+          );
+        });
+    } else {
+      return true;
+    }
+  });
 }
 
 
-let serverPort = process.env.PORT || 80;
+function initDb() {
+	// for each table required, check if already existing
+	// if not, create and populate
+	initDoctorsTable();
+	initLocationsTable();
+	initServicesTable();
+	initServicesLocationsTable();
 
-let petsList = require("./other/doctorsdata.json");
-let petsList = require("./other/locationsdata.json");
-let petsList = require("./other/servicesdata.json");
-let petsList = require("./other/serviceslocationsdata.json");
+	return true; 
+}
 
 /////////////////////////////////////////////
 ////////////////// APP.USE //////////////////
@@ -45,18 +156,55 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 /////////////////////////////////////////////
-////////////////// APP.USE //////////////////
+////////////////// APP.GET //////////////////
 /////////////////////////////////////////////
+
+// Name of the tables are:
+// doctors
+// locations
+// services
+// servicesLocations
 
 // Register REST entry points
 
-// app.get("/***", function(req, res) {
+app.get("/doctors/:id", function(req, res) {
+	let myQuery = sqlDb("doctors");
+	myQuery.where("id",1);
+	myQuery.then(result => {
+		res.send(JSON.stringify(result));
+	})
+})
 
+app.get("/locations/:id", function(req, res) {
+	let myQuery = sqlDb("locations");
+	myQuery.where("id",1);
+	myQuery.then(result => {
+		res.send(JSON.stringify(result));
+	})
+})
+
+app.get("/services/:id", function(req, res) {
+	let myQuery = sqlDb("services");
+	myQuery.where("id",1);
+	myQuery.then(result => {
+		res.send(JSON.stringify(result));
+	})
+})
+
+app.get("/servicesbylocations/:id", function(req, res) {
+	let myQuery = sqlDb("servicesLocations");
+	myQuery.where("idLocation",1);
+	myQuery.then(result => {
+		res.send(JSON.stringify(result));
+	})
+})
 
 
 /////////////////////////////////////////////
 /////////////////// INIT ////////////////////
 /////////////////////////////////////////////
+
+let serverPort = process.env.PORT || 5000;
   
 app.set("port", serverPort);
 
@@ -66,3 +214,14 @@ initDb();
 app.listen(serverPort, function() {
   console.log(`Your app is ready at port ${serverPort}`);
 });
+
+/*
+
+/doctors
+/doctors/:id
+/services/:id 
+/doctorsbyservice/:id
+/locations/:id
+/servicesbylocations/:id 
+
+*/
