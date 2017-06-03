24a707a4d1e48dfaa08ba8707785666673e9f275
Spranga <drew.taglia@gmail.com>
2017-06-03 13:11:11 +0200

    aggiunto form funzionante su prova form.html

---
+++ public/assets/js/forms/contact.js
@@ -0,0 +1,83 @@
+var ContactForm = function () {
+
+    return {
+        
+        //Contact Form
+        initContactForm: function () {
+	        // Validation
+	        $("#sky-form3").validate({
+	            // Rules for form validation
+	            rules:
+	            {
+	                name:
+	                {
+	                    required: true
+	                },
+	                email:
+	                {
+	                    required: true,
+	                    email: true
+	                },
+	                message:
+	                {
+	                    required: true,
+	                    minlength: 10
+	                },
+	                captcha:
+	                {
+	                    required: true,
+	                    remote: '#'
+	                }
+	            },
+	                                
+	            // Messages for form validation
+	            messages:
+	            {
+	                name:
+	                {
+	                    required: 'Inserisci il tuo nome',
+	                },
+	                email:
+	                {
+	                    required: 'Inerisci la tua email',
+	                    email: 'Inserisci una email valida'
+	                },
+	                message:
+	                {
+	                    required: 'Inserisci il testo del messaggio'
+	                },
+	                captcha:
+	                {
+	                    required: 'Please enter characters',
+	                    remote: 'Correct captcha is required'
+	                }
+	            },
+	                                
+	            // Ajax form submition                  
+	            submitHandler: function(form)
+	            {
+	                $(form).ajaxSubmit(
+	                {
+	                    beforeSend: function()
+	                    {
+	                        $('#sky-form3 button[type="submit"]').attr('disabled', true);
+	                    },
+	                    success: function()
+	                    {
+	                    	console.log("successs");
+	                        $("#sky-form3").addClass('submited');
+	                    }
+	                });
+	            },
+	            
+	            // Do not change code below
+	            errorPlacement: function(error, element)
+	            {
+	                error.insertAfter(element.parent());
+	            }
+	        });
+        }
+
+    };
+    
+}();
